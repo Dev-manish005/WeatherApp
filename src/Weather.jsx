36541ee0fd79bd5ede7458ef77b144faad891765
Manish Pradhan <manish.j.p.005@gmail.com>
2025-07-12 14:21:10 +0530

    Weather app project complete with proper error handling...

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from './assets/search.png';
 import clear_icon from './assets/clear.png';
@@ -11,31 +11,88 @@ import wind_icon from './assets/wind.svg';
 
 
 const Weather = () => {
+  const[weatherdata,setweatherdata]=useState(false);
+  const inputref=useRef();
+  const allicon={
+    '01d':clear_icon,
+    '01n':clear_icon,
+    '02d':cloud_icon,
+    '02n':cloud_icon,
+    '03d':cloud_icon,
+    '03n':cloud_icon,
+    '04d':cloud_icon,
+    '04n':cloud_icon,
+    '09d':drizzle_icon,
+    '09n':drizzle_icon,
+    '10d':rain_icon,
+    '10n':rain_icon,
+    '11d':rain_icon,
+    '11n':rain_icon,
+    '13d':snow_icon,
+    '13n':snow_icon,    
+  }
+  const search =async(city)=>{
+    if(city===""){
+      alert("Enter city name");
+      return;
+    }
+    try{
+      const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}
+      &units=metric&appid=${import.meta.env.VITE_APP_ID}`
+
+      const response=await fetch(url);
+      const data=await response.json();
+      console.log(data);
+      if(!response.ok){
+        alert(data.message);
+      }
+      const icon=allicon[data?.weather[0]?.icon]||clear_icon;
+      setweatherdata({
+        humidity:data?.main?.humidity,
+        windspeed:data?.wind?.speed,
+        temperature:Math.floor(data?.main?.temp),
+        location:data?.name,
+        icon:icon
+      })
+    }
+    catch(error){
+      setweatherdata(false);
+    }
+  }
+  useEffect(()=>{
+    search("kalyan");
+  },[])
+
   return (
     <div className='weather'>
         <div className="search-bar">
-            <input type='text' placeholder='search'/>
-            <img src={search_icon} alt='search'/>
+            <input ref={inputref} type='text' placeholder='search'/>
+            <img src={search_icon} alt='search'onClick={()=>search(inputref.current.value)}/>
         </div>
-        <img src={clear_icon} alt="clear.img" className='Weather-icon'/>
-        <p className='temperature'>26°C</p>
-        <p className='location'>Mumbai</p>
+        {weatherdata?<>
+        <img src={weatherdata.icon} alt="weather.img" className='Weather-icon'/>
+        <p className='temperature'>{weatherdata.temperature}°C</p>
+        <p className='location'>{weatherdata.location}</p>
         <div className='weather-data'>
           <div className='col'>
             <img src={humidity_icon} alt="humidity" />
             <div>
-            <p>91%</p>
+            <p>{weatherdata.humidity}%</p>
             <span>humidity</span>
             </div>
           </div>
           <div className='col'>
             <img src={wind_icon} alt="wind" />
             <div>
-            <p>3 km/hr</p>
+            <p>{weatherdata.windspeed}km/hr</p>
             <span>wind speed</span>
             </div>
           </div>
         </div>
+        </>:<> 
+          
+        </>}
+        
     </div>
   )
 }
